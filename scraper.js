ed6115146a15a0697376f746957363be0187c8d5
Kevin <Imvooz@gmail.com>
2014-03-24 23:29:36 +1100

    Removed dependency for MongoJS, replaced with Mongoose. Created model for Listing. Changed function to save database indexes, now automagically disconnects database when all iterations of saving are done.

--- scraper.js
+++ scraper.js
@@ -1,14 +1,16 @@
 // htmlParser Test
 var databaseURL = "mongodb://Imvoo:Imvoo@oceanic.mongohq.com:10035/imvoo_database";
-var collections = ["Listings"];
+var collection = 'listings';
 
 var cheerio = require('cheerio'),
-	request = require('request');
-	db = require('mongojs').connect(databaseURL, collections);
+	request = require('request'),
+  mongoose = require('mongoose');
 
 var url = "http://osu.ppy.sh/pages/include/profile-history.php?u=949789&m=0";
 var scoreRegex = /\d*,?\d*,?\d* \(\w*\) \w+,?\w*,?\w*,?\w*,?\w*/g;
-var scores;
+
+mongoose.connect(databaseURL);
+var Listing = mongoose.model(collection, { datePlayed: Date, songName: String, songLink: String, songDifficulty: String, score: String });
 
 request(url, function (error, response, html) 
 {
@@ -18,31 +20,29 @@ request(url, function (error, response, html)
 
   	$('time.timeago').each(function(i, element) 
     {
+      var amount = $('time.timeago').length;
   		var nextTag = this.next().text();
   		var date = this.text();
   		var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
   		var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
   		var link = "http://osu.ppy.sh" + this.next().attr('href');
+
   		scores = scoreRegex.exec(html);
   		console.log(date + " " + name);
   		console.log(link);
   		console.log(scores[0] + " " + difficulty);
 
-  		db.Listings.save({datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty}, function(err, saved)
+      var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
+      newSong.save(function (err) 
       {
-        if (err || !saved)
-        {
-          console.log("Not Saved!");
-        }
+        if (err)
+          console.log("Error in saving!");
         else
-        {
-          console.log("User Saved!");
-        }
+          console.log("Saved entry!");
+
+        if (amount == i + 1)
+          mongoose.disconnect();
       });
 		});
 	}
-  else 
-  {
-    console.log("GET TO THE CHOPPA!");
-  }
 });
