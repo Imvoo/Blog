625a578cf5d954de920e28297cb06eb60bc88314
Kevin <Imvooz@gmail.com>
2014-03-24 23:31:17 +1100

    Changed spaces to tabs because SWAG.

--- scraper.js
+++ scraper.js
@@ -4,7 +4,7 @@ var collection = 'listings';
 
 var cheerio = require('cheerio'),
 	request = require('request'),
-  mongoose = require('mongoose');
+	mongoose = require('mongoose');
 
 var url = "http://osu.ppy.sh/pages/include/profile-history.php?u=949789&m=0";
 var scoreRegex = /\d*,?\d*,?\d* \(\w*\) \w+,?\w*,?\w*,?\w*,?\w*/g;
@@ -14,35 +14,35 @@ var Listing = mongoose.model(collection, { datePlayed: Date, songName: String, s
 
 request(url, function (error, response, html) 
 {
-  if (!error && response.statusCode == 200) 
-  {
-  	var $ = cheerio.load(html);
+	if (!error && response.statusCode == 200) 
+	{
+		var $ = cheerio.load(html);
 
-  	$('time.timeago').each(function(i, element) 
-    {
-      var amount = $('time.timeago').length;
-  		var nextTag = this.next().text();
-  		var date = this.text();
-  		var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
-  		var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
-  		var link = "http://osu.ppy.sh" + this.next().attr('href');
+		$('time.timeago').each(function(i, element) 
+		{
+			var amount = $('time.timeago').length;
+			var nextTag = this.next().text();
+			var date = this.text();
+			var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
+			var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
+			var link = "http://osu.ppy.sh" + this.next().attr('href');
 
-  		scores = scoreRegex.exec(html);
-  		console.log(date + " " + name);
-  		console.log(link);
-  		console.log(scores[0] + " " + difficulty);
+			scores = scoreRegex.exec(html);
+			console.log(date + " " + name);
+			console.log(link);
+			console.log(scores[0] + " " + difficulty);
 
-      var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
-      newSong.save(function (err) 
-      {
-        if (err)
-          console.log("Error in saving!");
-        else
-          console.log("Saved entry!");
+			var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
+			newSong.save(function (err) 
+			{
+				if (err)
+					console.log("Error in saving!");
+				else
+					console.log("Saved entry!");
 
-        if (amount == i + 1)
-          mongoose.disconnect();
-      });
+				if (amount == i + 1)
+					mongoose.disconnect();
+			});
 		});
 	}
 });
