27acd0ac16145cd940c07f16a58b35680826de40
Kevin <Imvooz@gmail.com>
2014-04-06 21:28:23 +1000

    Successfully disconnected database after saving entries.

--- scraper.js
+++ scraper.js
@@ -4,7 +4,8 @@ var collection = 'listings';
 
 var cheerio = require('cheerio'),
 	request = require('request'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	async = require('async');
 
 var url = "http://osu.ppy.sh/pages/include/profile-history.php?u=949789&m=0";
 var scoreRegex = /\d*,?\d*,?\d* \(\w*\) \w+,?\w*,?\w*,?\w*,?\w*/g;
@@ -13,37 +14,51 @@ var scores;
 mongoose.connect(databaseURL);
 var Listing = mongoose.model(collection, { datePlayed: Date, songName: String, songLink: String, songDifficulty: String, score: String });
 
-request(url, function (error, response, html) 
-{
-	if (!error && response.statusCode == 200) 
+async.series([
+	function(callback)
 	{
-		var $ = cheerio.load(html);
-
-		$('time.timeago').each(function(i, element) 
+		request(url, function (error, response, html) 
 		{
-			var amount = $('time.timeago').length;
-			var nextTag = this.next().text();
-			var date = this.text();
-			var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
-			var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
-			var link = "http://osu.ppy.sh" + this.next().attr('href');
-
-			scores = scoreRegex.exec(html);
-			console.log(date + " " + name);
-			console.log(link);
-			console.log(scores[0] + " " + difficulty);
-
-			var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
-			newSong.save(function (err) 
+			var count = 0;
+			if (!error && response.statusCode == 200) 
 			{
-				if (err)
-					console.log("Error in saving!");
-				else
-					console.log("Saved entry!");
+				var $ = cheerio.load(html);
 
-				if (amount == i + 1)
-					mongoose.disconnect();
-			});
-		});
+				$('time.timeago').each(function(i, element) 
+				{
+					var amount = $('time.timeago').length;
+					var nextTag = this.next().text();
+					var date = this.text();
+					var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
+					var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
+					var link = "http://osu.ppy.sh" + this.next().attr('href');
+
+					scores = scoreRegex.exec(html);
+					console.log(date + " " + name);
+					console.log(link);
+					console.log(scores[0] + " " + difficulty);
+
+					var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
+					newSong.save(function (err) 
+					{
+						count++;
+						if (err)
+							console.log("Error in saving!");
+						else
+							console.log("Saved entry!");
+
+						if ($('time.timeago').length == count)
+							callback();
+					});
+				});
+			}
+		})
+	},
+
+	function(callback)
+	{
+		mongoose.disconnect();
+		console.log("Disconnected!");
+		callback();
 	}
-});
+])
