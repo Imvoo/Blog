182fd894712fac117938552da229567dde27f6ed
Kevin <Imvooz@gmail.com>
2014-04-06 22:17:52 +1000

    Only adds song if play-time is after latest listing.

--- scraper.js
+++ scraper.js
@@ -5,7 +5,8 @@ var collection = 'listings';
 var cheerio = require('cheerio'),
 	request = require('request'),
 	mongoose = require('mongoose'),
-	async = require('async');
+	async = require('async'),
+	moment = require('moment');
 
 var url = "http://osu.ppy.sh/pages/include/profile-history.php?u=949789&m=0";
 var scoreRegex = /\d*,?\d*,?\d* \(\w*\) \w+,?\w*,?\w*,?\w*,?\w*/g;
@@ -22,33 +23,49 @@ async.series([
 			var count = 0;
 			if (!error && response.statusCode == 200) 
 			{
-				var $ = cheerio.load(html);
+				Listing.findOne({}).sort("-datePlayed").exec(function(err, result) {
+					if (result != null)
+						console.log(result.datePlayed + " " + result.songName);
 
-				$('time.timeago').each(function(i, element) 
-				{
-					var amount = $('time.timeago').length;
-					var nextTag = this.next().text();
-					var date = this.text();
-					var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
-					var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
-					var link = "http://osu.ppy.sh" + this.next().attr('href');
+					var $ = cheerio.load(html);
 
-					scores = scoreRegex.exec(html);
-					console.log(date + " " + name);
-					console.log(link);
-					console.log(scores[0] + " " + difficulty);
-
-					var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
-					newSong.save(function (err) 
+					$('time.timeago').each(function(i, element) 
 					{
-						count++;
-						if (err)
-							console.log("Error in saving!");
-						else
-							console.log("Saved entry!");
+						var date = this.text();
+						if (result != null && (moment(date).isBefore(result.datePlayed) || moment(date).isSame(result.datePlayed)))
+						{
+							count++;
+							console.log("Already recorded, skipping!");
+							console.log(moment(date).diff(result.datePlayed));
 
-						if ($('time.timeago').length == count)
-							callback();
+							if ($('time.timeago').length == count)
+								callback();
+							return;
+						}
+
+						var amount = $('time.timeago').length;
+						var nextTag = this.next().text();
+						var name = nextTag.substr(0, nextTag.indexOf('[') - 1);
+						var difficulty = nextTag.substr(nextTag.indexOf('[') + 1, nextTag.length - nextTag.indexOf('[') - 2);
+						var link = "http://osu.ppy.sh" + this.next().attr('href');
+
+						scores = scoreRegex.exec(html);
+						console.log(date + " " + name);
+						console.log(link);
+						console.log(scores[0] + " " + difficulty);
+
+						var newSong = new Listing({ datePlayed: date, songName: name, songLink: link, songDifficulty: difficulty, score: scores[0] });
+						newSong.save(function (err) 
+						{
+							count++;
+							if (err)
+								console.log("Error in saving!");
+							else
+								console.log("Saved entry!");
+
+							if ($('time.timeago').length == count)
+								callback();
+						});
 					});
 				});
 			}
