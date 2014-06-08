6ee92f92868e33a056b1029edc3bbe4e81f8796d
Kevin Imvoo <Imvooz@gmail.com>
2014-06-08 13:05:55 +1000

    Commented everything, cleaned up some magic numbers.

--- express.js
+++ express.js
@@ -1,18 +1,31 @@
+//	Express.js
+//	This class is the implementation of the server, handling the connections
+//	made to the server and displaying the formatted HTML page in the end.
+
+//	Initialization of starting variables.
+//	Note, process.env.PORT is the randomly assigned port used by Heroku.
+//	A CronJob is also used to handle the updating of the database so that
+//	it doesn't rely on any external schedulers.
 var express = require('express');
-
 var app = express();
-app.use(express.compress());
+var port = Number(process.env.PORT || 5000);
+var CronJob = require('cron').CronJob;
+var retriever = require('./retriever');
+var user = "949789";
+var retrieverDelay = 5;
 
+retriever.connect();
+
+//	Setup the server to compress assets with GZIP and locate the server's
+//	files.
+app.use(express.compress());
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/public/jade');
 app.use(express.static(__dirname + '/public'));
 
-var port = Number(process.env.PORT || 5000);
-
-var CronJob = require('cron').CronJob;
-var retriever = require('./retriever');
-var user = "949789";
-
+//	TODO: Add wildcard modifier to handle separate player requests.
+//	This handles the actual connections to the server, grabbing the given user's
+//	avatar and scores.
 app.get('/', function(req, res) 
 {
 	retriever.getAvatar(user, function() {
@@ -28,13 +41,17 @@ app.get('/', function(req, res)
 	});
 });
 
+//	Actually starts the server up and logs it to the server.
 app.listen(port);
 console.log("Server has started on port: " + port + "!");
 
+//	In the event the server tries to disconnect, disconnect the connection
+//	to the database too.
 process.on('SIGTERM', function () {
 	retriever.disconnect();
 });
 
-var job = new CronJob('5 * * * * *', function() {
+//	This handles the updating of database entries with a delay set.
+var job = new CronJob('* ' + retrieverDelay + ' * * * *', function() {
 	retriever.update();	
 }, null, true)
